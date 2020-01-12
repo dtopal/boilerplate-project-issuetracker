2a5a62754c1037ee969418e30385e7b30653c9f6
dtopal <11358089+dtopal@users.noreply.github.com>
2020-01-12 15:50:22 +0900

    Add API routes for CRUD operations

--- routes/api.js
+++ routes/api.js
@@ -16,26 +16,113 @@ const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRIN
 
 module.exports = function (app) {
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .post(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .put(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      var project = req.params.project;
-      
-    });
-    
+  MongoClient.connect(CONNECTION_STRING, (err, db) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('db connection successful');
+
+      app.route('/api/issues/:project')
+
+      .get(function (req, res){
+        var project = req.params.project;
+        db.collection(project).find({}).filter(req.query).toArray(function(err, data){
+          if (err) {
+            console.log(err);
+          } else {
+            //console.log(data);
+            res.send(data);
+          }
+        });
+
+      })
+
+      .post(function (req, res){
+        var project = req.params.project;
+        //console.log(req.query); //query always present just empty
+        //check for required fields
+        if (!req.body.issue_title || !req.body.issue_text || !req.body.created_by) {
+          res.send('Missing required fields');
+          return;
+        }
+        var time = new Date();
+        db.collection(project).insertOne({
+          issue_title: req.body.issue_title,
+          issue_text: req.body.issue_text,
+          created_by: req.body.created_by,
+          assigned_to: req.body.assigned_to || '',
+          status_text: req.body.status_text || '',
+          open: true,
+          created_on: time,
+          updated_on: time
+        }, (err, data) => {
+          if (err) {
+            res.send('ERROR -- please try again');
+          } else {
+            res.json(data.ops[0]);
+          }
+        });
+      })
+
+      .put(function (req, res){
+        var project = req.params.project;
+        //console.log(req.body);
+        //No id sent
+        if (!req.body._id) {
+          res.send('No ID sent');
+          return;
+        }
+        if (req.body._id && Object.keys(req.body).length == 1) {
+          res.send('No updated field sent');
+          return;
+        }
+        var updateObj = {};
+        updateObj.updated_on = new Date();
+        Object.keys(req.body).map(x => {
+          if (x !== '_id') {
+            updateObj[x] = req.body[x];
+          }
+        })
+        //console.log(updateObj);
+        db.collection(project).findOneAndUpdate(
+          { _id: ObjectId(req.body._id) },
+          { $set: updateObj },
+          { returnOriginal: false },
+          function(err, data){
+            if (err) {
+              console.log(err);
+            } else {
+              //console.log(data.value);
+              res.json(data.value);
+            }
+          }
+        )
+      })
+
+      .delete(function (req, res){
+        var project = req.params.project;
+        if (!req.body._id) {
+          res.send('_id error');
+          return;
+        }
+        db.collection(project).findOneAndDelete({ _id: ObjectId(req.body._id) }, function(err, data){
+          if (err) {
+            //console.log(err);
+            res.send('could not delete ' + req.body._id);
+          } else {
+            //console.log(data);
+            res.send('deleted ' + data.value._id);
+          }
+        });
+
+      });
+
+      //404 Not Found Middleware
+      app.use(function(req, res, next) {
+        res.status(404)
+          .type('text')
+          .send('Not Found');
+      });
+    }
+  })
 };
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+var helmet      = require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -11,11 +12,13 @@ var runner            = require('./test-runner');
 
 var app = express();
 
+
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
-
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,15 +38,8 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
+//Routing for API
+apiRoutes(app);
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
